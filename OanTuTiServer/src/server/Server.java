dd77534ff84a79649f27dde967cc44d5522044ff
duy <duydq2k1@gmail.com>
2022-10-12 17:54:42 +0700

    init base

---
+++ OanTuTiServer/src/server/Server.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author Hp
+ */
+public class Server {
+
+    public static Socket connect = null;
+
+    public static void main(String[] args) {
+        ServerSocket listener = null;
+        int clientNumber = 0;
+        try {
+            listener = new ServerSocket(8888);
+        } catch (IOException e) {
+            System.out.println(e);
+            System.exit(1);
+        }
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                10, // corePoolSize
+                100, // maximumPoolSize
+                10, // thread timeout
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(8) // queueCapacity
+        );
+        try {
+            while (true) {
+                // Chấp nhận một yêu cầu kết nối từ phía Client.
+                // Đồng thời nhận được một đối tượng Socket tại server.
+                connect = listener.accept();
+//                ServerThread serverThread = new ServerThread(connect, clientNumber++);
+//                serverThreadBus.add(serverThread);
+//                System.out.println("Số thread đang chạy là: "+serverThreadBus.getLength());
+//                executor.execute(serverThread);
+                
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                listener.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
